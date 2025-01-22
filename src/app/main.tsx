a2caac393ecde198d9ab02cae8c5d55a91f55300
junhee <limjunhee4575@gmail.com>
2025-01-22 17:22:57 +0900

    feat : helmet설정

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,16 @@
 import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </HelmetProvider>,
   // </StrictMode>,
 );
