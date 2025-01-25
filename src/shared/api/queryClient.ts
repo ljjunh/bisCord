3e64dd6d462d71f794ea2e861aa35a4c3b73f3c7
junhee <limjunhee4575@gmail.com>
2025-01-25 16:42:18 +0900

    feat : 쿼리캐시 설정

--- src/shared/api/queryClient.ts
+++ src/shared/api/queryClient.ts
@@ -1,4 +1,5 @@
-import { QueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -9,4 +10,12 @@ export const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
+  queryCache: new QueryCache({}),
+  mutationCache: new MutationCache({
+    onError: (error, _requestData, _context, mutation) => {
+      if (!mutation.meta?.ignoreToast) {
+        toast.error(error.message);
+      }
+    },
+  }),
 });
