4aa3b31b9e5e0da8d83faa5a376189208149e082
junhee <limjunhee4575@gmail.com>
2025-02-01 20:03:48 +0900

    feat : dm-create

--- src/features/directMessage/ui/DMView.tsx
+++ src/features/directMessage/ui/DMView.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useMemo, useRef, useState } from 'react';
-import { useInfiniteQuery } from '@tanstack/react-query';
-import type { Message } from '../model/types';
+import { useInfiniteQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { QUERY_KEYS } from '@/shared/api/queryKeys';
 import { useInfiniteScroll } from '@/shared/lib/useInfiniteScroll';
 import { DMQueries } from '../api/queries';
 import { groupMessages } from '../lib/utils';
@@ -9,11 +9,21 @@ import { MessageGroup } from './MessageGroup';
 
 export const DMView = () => {
   const { id } = useParams<{ id: string }>();
+  const queryClient = useQueryClient();
 
   const messageContainerRef = useRef<HTMLDivElement>(null);
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
     ...DMQueries.getDM({ otherUserId: Number(id) }),
+  });
+
+  const { mutate } = useMutation({
+    ...DMQueries.postDM,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.directMessage.detail({ otherUserId: 6 }),
+      });
+    },
   });
 
   const observerRef = useInfiniteScroll({
@@ -22,15 +32,13 @@ export const DMView = () => {
     isLoading: isFetchingNextPage,
   });
 
-  const allMessages = data?.pages.flatMap((page) => page.chats.content) ?? [];
-
-  const [realtimeMessages, setRealtimeMessages] = useState<Message[]>([]);
-
   const messages = useMemo(() => {
-    return [...allMessages, ...realtimeMessages].sort(
+    const flatMessages = data?.pages.flatMap((page) => page.chats.content) ?? [];
+
+    return flatMessages.sort(
       (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
     );
-  }, [allMessages, realtimeMessages]);
+  }, [data]);
 
   const messageGroups = useMemo(() => groupMessages(messages), [messages]);
 
@@ -46,17 +54,8 @@ export const DMView = () => {
     e.preventDefault();
     if (!newMessage.trim()) return;
 
-    const newMsg: Message = {
-      chatId: Date.now().toString(),
-      content: newMessage,
-      userId: 1, // 현재 사용자 ID
-      name: 'currentUser', // 현재 사용자 이름
-      profileImageUrl: null,
-      createdAt: new Date().toISOString(),
-      updated: false,
-    };
+    mutate({ recipientId: Number(id), content: newMessage });
 
-    setRealtimeMessages((prev) => [...prev, newMsg]);
     setNewMessage('');
   };
 
