ac1def95268135f8b73ea73f82a2d7b74fc428eb
junhee <limjunhee4575@gmail.com>
2025-02-18 18:20:59 +0900

    feat : not-found

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@/shared/model/constants/routes';
+import { EmptyBgIcon } from '@/shared/ui/icons/EmptyBgIcon';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate(ROUTES.ROOT);
+  };
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center space-y-6 bg-mid-gray text-white">
+      <EmptyBgIcon />
+      <h1 className="text-2xl font-bold">페이지를 찾을 수 없어요!</h1>
+      <button
+        onClick={handleButtonClick}
+        className="rounded-md bg-blue-purple px-6 py-2 text-white duration-200 hover:bg-blue"
+      >
+        홈으로 돌아가기
+      </button>
+    </div>
+  );
 };
 
 export default NotFound;
