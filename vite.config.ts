b2298cc9186d9e1159943280b50dcd91671ed2d0
junhee <limjunhee4575@gmail.com>
2025-03-21 17:34:22 +0900

    refactor : 대체폰트추가-프리로드적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        tags: [
+          {
+            injectTo: 'head-prepend',
+            tag: 'link',
+            attrs: {
+              rel: 'preload',
+              href: 'src/assets/fonts/subset-PretendardVariable-Regular.woff2',
+              as: 'font',
+              type: 'font/woff2',
+              crossorigin: 'anonymous',
+            },
+          },
+        ],
+      },
+    }),
     react(),
     visualizer({
       filename: 'stats.html',
